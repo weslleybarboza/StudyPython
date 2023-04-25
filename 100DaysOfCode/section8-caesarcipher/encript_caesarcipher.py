831d0c987fe90955142d8d7be7faf008dfd24876
Weslley Barboza <weslleybarboza@gmail.comm>
2023-04-25 23:00:33 +0200

    development

--- 100DaysOfCode/section8-caesarcipher/encript_caesarcipher.py
+++ 100DaysOfCode/section8-caesarcipher/encript_caesarcipher.py
@@ -1,39 +1,35 @@
+import art
+
+print(art.logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-len(alphabet)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-# text = 'hfxf'
-
-# def encrypt(text, shift):
-#     new_text = ""
-#     for l in text:
-#         new_index = alphabet.index(l) + shift
-#         new_text += alphabet[new_index]
-#     print(f'The encoded text is {new_text}.')
-
-# def decrypt(text, shift):
-#     new_text = ""
-#     for l in text:
-#         new_index = alphabet.index(l) - shift
-#         new_text += alphabet[new_index]
-#     print(f'The decode text is {new_text}.')
+restart = True
 
 def caesar(direction, text, shift):
     new_text = ""
     if direction == 'decode':
         shift *= -1
-    for l in text:
-        new_index = alphabet.index(l) + shift
-        new_text += alphabet[new_index]
-    print(f'The {direction}d text is {new_text}.')    
+    for l in text:       
+        if l in alphabet:
+            new_index = alphabet.index(l) + shift
+            word = alphabet[new_index]
+        else:
+            word = l
+        new_text += word
+    print(f'The {direction}d text is "{new_text}".')    
 
-caesar(direction, text, shift)
+while restart:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
 
-# if direction == 'encode':
-#     encrypt(text,shift)
-# else:
-#     decrypt(text, shift)
+    caesar(direction, text, shift)
+
+    chose = input("\nDo you want to restart the cipher program? Y or N. ").lower()
+
+    if chose == 'n':
+        restart = False
+        print("Goodbye")
