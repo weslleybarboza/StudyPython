d2e5cda018eb5c97ed7fc4d5e93a16e373a2f0e4
Weslley Barboza <weslleybarboza@gmail.comm>
2023-05-10 06:50:34 +0200

    desenvolvimento

--- 100DaysOfCode/section11-blackjack/main.py
+++ 100DaysOfCode/section11-blackjack/main.py
@@ -17,3 +17,36 @@
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
 
+from art import logo
+import cards as c
+
+# input("Do you want to play a game of Blackjack? Type 'y'or 'n': ").lower
+print(logo)
+
+hand_players = {
+    "player" : [],
+    "computer" : []
+}
+
+#start the game with score < 21
+
+# def add_cards_player(player, qty):
+
+# while init_grant_than_21:
+#     c.add_new_cards_hand(hand, qty)
+#     score = sum(hand)
+#     if score < 21:
+#         init_grant_than_21 = False
+#     return hand
+
+qty = 2
+for player in hand_players:
+    score = 0
+    while score < 21:
+        for card in range(0, qty):
+            hand_players[player].append(c.get_random_cards())
+            score = sum(hand_players[player])
+
+print(hand_players)
+
+# print(f"    Your cards: {card_hand}, current score: {score}.")
