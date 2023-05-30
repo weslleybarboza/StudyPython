1f5518ddd1d43850334bdb5e30c5ccee8adee710
Weslley Barboza <weslleybarboza@gmail.com>
2023-05-30 07:07:51 +0200

    desenvolvimento

--- 100DaysOfCode/section14-HigherLowerGame/main.py
+++ 100DaysOfCode/section14-HigherLowerGame/main.py
@@ -1,7 +1,7 @@
 # loop start
 
 
-from random import choice
+from random import choice as random_choice
 from game_data import data
 
 list_used = []
@@ -15,19 +15,19 @@ def newOption ():
 
     list_available = [item for item in list_option if item not in list_used]
     
-    new_option = choice(list_available)
+    new_option = random_choice(list_available)
     list_used.append(new_option)
     
     return new_option
 
-def result(a_option, b_option, choice):
+def result(a_option, b_option):
     '''
-    Check if choice is correct. Return 1 for True and 0 False.
+    Return granter option.
     '''
-    if a_option['follower_count'] >= b_option['follower_count'] and choice == 'a':
-        return 1
+    if a_option['follower_count'] >= b_option['follower_count']:
+        return 'a'
     else:
-        return 0
+        return 'b'
 
 # random in list, show option A var a_option.
 a_option = newOption()
@@ -35,25 +35,35 @@ a_option = newOption()
 # random in list, show B option var b_option. Can't be A.
 b_option = newOption()
 
-print(f"Compare A: {a_option['name']}, a {a_option['description']}, from {a_option['country']}.")
-print(f"Against B: {b_option['name']}, a {b_option['description']}, from {b_option['country']}.")
+score = 0
+playing = True
+while playing:
+    print(f"Compare A: {a_option['name']}, a {a_option['description']}, from {a_option['country']}.")
+    print(f"Against B: {b_option['name']}, a {b_option['description']}, from {b_option['country']}.")
 
-# show option to user choose. Save it in var user_option.
-choice = input("Who has more follower? Type 'A' or 'B': ").lower()
+    # show option to user choose. Save it in var user_option.
+    choice = input("Who has more follower? Type 'A' or 'B': ").lower()
+    print(f'You choose: {choice}')
 
-# compare the options, show who won. funcao winner().
-result(a_option, b_option, choice)
+    # compare the options, show who won. funcao winner().
+    outcome = result(a_option, b_option)
+
+    # def check_result(choice, result):
+    if choice == outcome:
+        score += 1
+        if outcome == 'a':
+            b_option = newOption()
+        else:
+            a_option = b_option
+            b_option = newOption()
+    else:
+        print(score)
+        print('game over')
+        playing = False
 
 
-
-
-
-
-
-
-print(list_used)
-
-
+# print(list_used)
 
 # if user_option won, the var a_option receive winner(), the var b_option receive another random. The score gets 1 point. The game continue.
+
 # if user_option lose, the game ended, the final score is showed.
