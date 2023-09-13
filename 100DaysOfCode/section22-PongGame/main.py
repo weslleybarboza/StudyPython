08cb0c0c2c8e3ff5b56532f5f031510fe4c26547
Weslley Barboza <weslleybarboza@gmail.com>
2023-09-13 07:12:05 +0200

    study

--- 100DaysOfCode/section22-PongGame/main.py
+++ 100DaysOfCode/section22-PongGame/main.py
@@ -1,37 +1,60 @@
 from turtle import Screen
-from trace import Trace
 from player import Player
+from ball import Ball
+from time import sleep
 
 SCREEN_HEIGHT = 1000
 SCREEN_WIDTH = 1500
 SCREEN_COLOR = "black"
 SCREEN_TITLE = "Welcome to Pong Game"
-
+POSITION_X_START_LEFT = -720
+POSITION_X_START_RIGHT= 720
 
 screen = Screen()
 screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 screen.title(SCREEN_TITLE)
 screen.bgcolor(SCREEN_COLOR)
-
-
-# TODO: create the screen with vertical line
-# trace = Trace()
-# TODO: create the player1 object - Player. Allow to control
-
-player = Player()
-
-screen.onkey(player.move_up, "Up")
-screen.onkey(player.move_down, "Down")
 screen.listen()
 
-# TODO: CREATE THE player2 object - PC
+l_paddle = Player(position_x=POSITION_X_START_LEFT)
+r_paddle = Player(position_x=POSITION_X_START_RIGHT)
+ball = Ball()
 
-computer = Player()
+screen.onkey(r_paddle.move_up, "Up")
+screen.onkey(r_paddle.move_down, "Down")
 
-# TODO: create the boll
-# TODO: detect the collision with wall and bounce
-# TODO: detect the collision with paddle
+screen.onkey(l_paddle.move_up, "w")
+screen.onkey(l_paddle.move_down, "s")
+
 # TODO: detect when paddle misses
 # TODO: keep scoreboard
+
+
+game_is_on = True
+while game_is_on:
+    sleep(0.1)
+    screen.update()
+    ball.move()
+
+    print(f"L POSICAO: {l_paddle.pos()}")
+    print(f"R POSICAO: {r_paddle.pos()}")
+
+    # detect the collision with wall and bounce
+    y_limit_max = (SCREEN_HEIGHT * 0.95) / 2
+    y_limit_min = y_limit_max * -1
+    
+    if ball.ycor() > y_limit_max or ball.ycor() < y_limit_min:
+        ball.bounce()
+
+# TODO: detect the collision with paddle
+
+    print(r_paddle.pos())
+    print(ball.position())
+    print(f'colision - dist: {ball.distance(r_paddle)}')
+
+    
+    # if ball.distance(r_paddle) < 50:
+    #     print(f'colision - dist: {ball.distance(r_paddle)}')
+    game_is_on = False
 
 screen.exitonclick()
--- 100DaysOfCode/section22-PongGame/player.py
+++ 100DaysOfCode/section22-PongGame/player.py
@@ -1,32 +1,35 @@
 from turtle import Turtle
 
 COLOR = "white"
-POSITION_Y_PLAYER_START = -450
-POSITION_Y_PC_START = -450
-POSITION_X_START = -720
 POSITION_VARIATION = 40
+POSITION_Y_START = 0
 
+class Player(Turtle):
+    def __init__(self, position_x):
+        super().__init__()
 
-class Player:
-    def __init__(self):
         self.trace = Turtle(shape="square")
         self.trace.speed(9)
         self.trace.penup()
         self.trace.color(COLOR)
+        self.position_y = POSITION_Y_START
+        self.position_x = position_x
         self.set_player()
+        
 
     def set_player(self):
-        position_y = POSITION_Y_PLAYER_START
+        position_y = self.position_y
         self.trace.turtlesize(stretch_wid=4, stretch_len=0.6)
-        self.trace.setposition(POSITION_X_START, position_y)
+        self.trace.setposition(self.position_x , position_y)
+        print(f"SET PLAYER POSITION: {self.trace.pos()}")
 
     def move_up(self):
         position_y = self.trace.position()[1]
         new_position_y = position_y + POSITION_VARIATION
-        self.trace.setposition(POSITION_X_START, new_position_y)
+        self.trace.setposition(self.position_x , new_position_y)
 
     def move_down(self):
         position_y = self.trace.position()[1]
         new_position_y = position_y - POSITION_VARIATION
-        self.trace.setposition(POSITION_X_START, new_position_y)
+        self.trace.setposition(self.position_x , new_position_y)
         
